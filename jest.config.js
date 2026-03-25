c82b0bca0ae78c511c2592d7066b34112226edc4
Hezequiel <jansonquiel@gmail.com>
2026-03-25 01:44:55 -0300

    Removendo a linha extensionsToTreatAsEsm: ['.js']

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ export default {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.js'],
   transform: {},
-  extensionsToTreatAsEsm: ['.js'],
   setupFiles: ['./jest.setup.js'],
 };
